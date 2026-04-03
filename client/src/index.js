4696873668fb3eca9c2cfce6c195b1855f740bee
Ishwari Shevate <ishwarishevate@gmail.com>
2026-04-04 00:55:03 +0530

    removed PersistGate and persistor completely

--- client/src/index.js
+++ client/src/index.js
@@ -3,28 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './redux/store';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { PersistGate } from 'redux-persist/es/integration/react';
-import { persistor } from './redux/store';
 
 const theme = createTheme();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <ThemeProvider theme={theme}>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </ThemeProvider>
-    </PersistGate>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ThemeProvider>
   </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
